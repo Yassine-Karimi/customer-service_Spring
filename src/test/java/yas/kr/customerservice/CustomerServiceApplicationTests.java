d68bf0d435d1fd2f1ed257f67fdad9e930dd488f
Yassine Karimi <66490404+OchiHa@users.noreply.github.com>
2023-05-22 19:36:23 +0100

    micro service

--- src/test/java/yas/kr/customerservice/CustomerServiceApplicationTests.java
+++ src/test/java/yas/kr/customerservice/CustomerServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package yas.kr.customerservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
